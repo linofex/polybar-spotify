55ef65327fbe95ef74f2f84729d34321fffaa135
linofex <alessandro.noferi@gmail.com>
2021-07-02 15:15:24 +0200

    updated artist name with album name if artist is u"" (e.g in podcasts)

--- spotify_status.py
+++ spotify_status.py
@@ -115,10 +115,10 @@ try:
         play_pause = label_with_font.format(font=play_pause_font, label=play_pause)
 
     # Handle main label
-
-    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
-    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
     album = fix_string(metadata['xesam:album']) if metadata['xesam:album'] else ''
+    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'][0] else album
+    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
+    
 
     if (quiet and status == 'Paused') or (not artist and not song and not album):
         print('')
